4ba9441f39d40c4e9c9b266c7515d56447d40dd4
PatrickBett <patrickbett018@gmail.com>
2023-11-26 02:56:51 +0300

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    num_list = list()
+    
+    if length == 0:
+        print(list())
+    
+    for i in range(length):
+        if len(num_list) > 1:
+            next_number = num_list[-1] + num_list[-2]
+            num_list.append(next_number)
+            
+        else:
+            num_list.append(i)
+
+    print(num_list)
+print_fibonacci(9)
+          
